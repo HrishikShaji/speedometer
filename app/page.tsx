b35ebad4d6bf58a03fd8dc845192372441e5b5af
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-19 08:54:21 +0530

    speedometer design modified

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,22 @@ import { Speedometer } from "@/components/Speedometer";
 import { useState } from "react";
 
 export default function Home() {
-	const [speed, setSpeed] = useState(0);
-	return (
-		<main className="h-screen w-full flex flex-col gap-10 justify-center items-center bg-neutral-900">
-			<Speedometer speed={speed} />
-			<input
-				type="range"
-				defaultValue={speed}
-				min={0}
-				max={240}
-				onChange={(e) => setSpeed(parseInt(e.target.value))}
-			/>
-		</main>
-	);
+  const [speed, setSpeed] = useState(0);
+  return (
+    <main className=" w-full h-screen flex flex-col gap-3 p-2 justify-center items-center bg-neutral-900">
+      <div className="h-[720px]  w-[1080px] bg-neutral-700 flex flex-col items-center rounded-3xl relative">
+        <div className="flex gap-20 absolute bottom-10">
+          <Speedometer speed={speed} size={350} />
+          <Speedometer speed={speed} size={350} />
+        </div>
+      </div>
+      <input
+        type="range"
+        defaultValue={speed}
+        min={0}
+        max={240}
+        onChange={(e) => setSpeed(parseInt(e.target.value))}
+      />
+    </main>
+  );
 }
--- components/Speedometer.tsx
+++ components/Speedometer.tsx
@@ -1,53 +1,88 @@
 interface SpeedometerProps {
-	speed: number;
+  speed: number;
+  size: number;
 }
 
-export const Speedometer: React.FC<SpeedometerProps> = ({ speed }) => {
-	return (
-		<div className="relative w-[500px] h-[500px] rounded-full bg-black flex  items-center justify-center">
-			<div className="bg-black absolute z-50 top-[50%] left-[50%] p-2 rounded-full -translate-x-[50%] -translate-y-[50%] flex justify-center items-center">
-				<div className="rounded-full  bg-white h-6 w-6"></div>
-			</div>
-			<div className="bg-black absolute z-40 top-[50%] left-[50%] h-[300px] w-[300px] rounded-full -translate-x-[50%] -translate-y-[50%]"></div>
-			<div className="bg-black absolute z-20 top-[50%] left-[50%] h-[350px] w-[350px] rounded-full -translate-x-[50%] -translate-y-[50%]"></div>
-			<div className="bg-black absolute z-10 top-[50%] left-[50%] h-[400px] w-[400px] rounded-full -translate-x-[50%] -translate-y-[50%]"></div>
-			{Array.from({ length: 9 }).map((_, i) => (
-				<div
-					key={i}
-					style={{ rotate: `${330 + i * 30}deg` }}
-					className="w-[500px] flex z-30  h-[6px] absolute "
-				>
-					<div className="w-[50%] h-full bg-white"></div>
-					<div className="w-[50%] h-full bg-transparent"></div>
-				</div>
-			))}
-			{Array.from({ length: 16 }).map((_, i) => (
-				<div
-					key={i}
-					style={{ rotate: `${330 + i * 15}deg` }}
-					className="w-full flex   h-[4px] absolute   "
-				>
-					<div className="w-[50%] h-full bg-white"></div>
-					<div className="w-[50%] h-full bg-transparent"></div>
-				</div>
-			))}
-			{Array.from({ length: 80 }).map((_, i) => (
-				<div
-					key={i}
-					style={{ rotate: `${330 + i * 3}deg` }}
-					className="w-full flex   h-[1px] absolute   "
-				>
-					<div className="w-[50%] h-full bg-white"></div>
-					<div className="w-[50%] h-full bg-transparent"></div>
-				</div>
-			))}
-			<div
-				className=" w-[150px] rounded-3xl overflow-hidden flex h-[10px]  z-40  "
-				style={{ rotate: `${330 + speed}deg` }}
-			>
-				<div className="h-full w-[50%] bg-blue-500"></div>
-				<div className="h-full w-[50%] bg-transparent"></div>
-			</div>
-		</div>
-	);
+export const Speedometer: React.FC<SpeedometerProps> = ({ speed, size }) => {
+  return (
+    <div
+      style={{ height: `${size}px`, width: `${size}px` }}
+      className="relative  rounded-full bg-black flex  items-center justify-center"
+    >
+      <div className="bg-black absolute z-50  p-2 rounded-full  flex justify-center items-center">
+        <div className="rounded-full  bg-white h-6 w-6"></div>
+      </div>
+      <div
+        style={{ height: `${size - 100}px`, width: `${size - 100}px` }}
+        className="bg-black absolute z-40   rounded-full "
+      >
+        <div className="h-full w-full">
+          <div className="relative w-full h-full flex items-center justify-center">
+            {Array.from({ length: 9 }).map((_, i) => (
+              <div
+                key={i}
+                style={{
+                  rotate: `${(i + 1) * 30 + 102}deg`,
+                  transform: `translate(95px,30px)`,
+                }}
+                className="absolute    rounded-full"
+              >
+                <h1
+                  className="text-white text-xl "
+                  style={{ rotate: `-${(i + 1) * 30 + 102}deg` }}
+                >
+                  {i * 20}
+                </h1>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div
+        style={{ height: `${size - 100}px`, width: `${size - 100}px` }}
+        className="bg-black main absolute z-20 top-[50%] left-[50%]  rounded-full -translate-x-[50%] -translate-y-[50%]"
+      ></div>
+      <div
+        style={{ height: `${size - 50}px`, width: `${size - 50}px` }}
+        className="bg-black absolute z-10 top-[50%] left-[50%]  rounded-full -translate-x-[50%] -translate-y-[50%]"
+      ></div>
+      {Array.from({ length: 9 }).map((_, i) => (
+        <div
+          key={i}
+          style={{ rotate: `${330 + i * 30}deg` }}
+          className="w-full flex z-30  h-[6px] absolute "
+        >
+          <div className="w-[50%] h-full bg-white"></div>
+          <div className="w-[50%] h-full bg-transparent"></div>
+        </div>
+      ))}
+      {Array.from({ length: 16 }).map((_, i) => (
+        <div
+          key={i}
+          style={{ rotate: `${330 + i * 15}deg` }}
+          className="w-full flex   h-[4px] absolute   "
+        >
+          <div className="w-[50%] h-full bg-white"></div>
+          <div className="w-[50%] h-full bg-transparent"></div>
+        </div>
+      ))}
+      {Array.from({ length: 80 }).map((_, i) => (
+        <div
+          key={i}
+          style={{ rotate: `${330 + i * 3}deg` }}
+          className="w-full flex   h-[1px] absolute   "
+        >
+          <div className="w-[50%] h-full bg-white"></div>
+          <div className="w-[50%] h-full bg-transparent"></div>
+        </div>
+      ))}
+      <div
+        className=" w-[150px] rounded-3xl overflow-hidden flex h-[6px]  z-40  "
+        style={{ rotate: `${330 + speed}deg` }}
+      >
+        <div className="h-full w-[50%] bg-blue-500"></div>
+        <div className="h-full w-[50%] bg-transparent"></div>
+      </div>
+    </div>
+  );
 };
