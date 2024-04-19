7fbf83629d661bc170e92ce812921a5e08228683
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-19 22:38:24 +0530

    added Meterseven

--- app/page.tsx
+++ app/page.tsx
@@ -3,56 +3,62 @@ import { One } from "@/components/One";
 import { useEffect, useState } from "react";
 import gsap from "gsap";
 import { MeterOne } from "@/components/MeterOne";
+import { MeterTwo } from "@/components/MeterTwo";
+import { MeterThree } from "@/components/MeterThree";
+import { MeterFour } from "@/components/MeterFour";
+import { MeterFive } from "@/components/MeterFive";
+import { MeterSix } from "@/components/MeterSix";
+import { MeterSeven } from "@/components/MeterSeven";
 
 export default function Home() {
-  const [speed, setSpeed] = useState(0);
+	const [speed, setSpeed] = useState(0);
 
-  useEffect(() => {
-    const needle = document.getElementById("needl");
-    const el = document.getElementById("fifth-one");
+	useEffect(() => {
+		const needle = document.getElementById("needl");
+		const el = document.getElementById("fifth-one");
 
-    if (speed > 40 && speed < 90) {
-      gsap.to(el, { fill: "green" });
-    } else {
-      gsap.to(el, { fill: "white" });
-    }
+		if (speed > 40 && speed < 90) {
+			gsap.to(el, { fill: "green" });
+		} else {
+			gsap.to(el, { fill: "white" });
+		}
 
-    gsap.fromTo(
-      needle,
-      {
-        rotate: 0,
-      },
-      {
-        transformOrigin: "bottom left",
-        rotate: 360,
-        duration: 3,
-      },
-    );
-  }, [speed]);
-  return (
-    <main className=" w-full h-screen flex flex-col gap-3 p-2 justify-center items-center bg-neutral-900">
-      <div className="h-[720px]  w-[1080px] bg-neutral-700 flex flex-col justify-center items-center rounded-3xl relative">
-        <div className="absolute top-5 right-5 bg-neutral-400 rounded-3xl w-10 h-5"></div>
-        <div className="flex gap-40 absolute ">
-          <div className="w-[500px]">
-            <MeterOne speed={speed} />
-          </div>
-        </div>
-        <div className="flex gap-5 absolute bottom-10">
-          <div className="bg-neutral-400 rounded-full w-10 h-10"></div>
-          <div className="bg-neutral-400 rounded-full w-10 h-10"></div>
-          <div className="bg-neutral-400 rounded-full w-10 h-10"></div>
-          <div className="bg-neutral-400 rounded-full w-10 h-10"></div>
-          <div className="bg-neutral-400 rounded-full w-10 h-10"></div>
-        </div>
-      </div>
-      <input
-        type="range"
-        defaultValue={speed}
-        min={0}
-        max={240}
-        onChange={(e) => setSpeed(parseInt(e.target.value))}
-      />
-    </main>
-  );
+		gsap.fromTo(
+			needle,
+			{
+				rotate: 0,
+			},
+			{
+				transformOrigin: "bottom left",
+				rotate: 360,
+				duration: 3,
+			},
+		);
+	}, [speed]);
+	return (
+		<main className=" w-full h-screen flex flex-col gap-3 p-2 justify-center items-center bg-neutral-900">
+			<div className="h-[720px]  w-[1080px] bg-neutral-700 flex flex-col justify-center items-center rounded-3xl relative">
+				<div className="absolute top-5 right-5 bg-neutral-400 rounded-3xl w-10 h-5"></div>
+				<div className="flex gap-40 absolute ">
+					<div className="w-[500px]">
+						<MeterSeven speed={speed} />
+					</div>
+				</div>
+				<div className="flex gap-5 absolute bottom-10">
+					<div className="bg-neutral-400 rounded-full w-10 h-10"></div>
+					<div className="bg-neutral-400 rounded-full w-10 h-10"></div>
+					<div className="bg-neutral-400 rounded-full w-10 h-10"></div>
+					<div className="bg-neutral-400 rounded-full w-10 h-10"></div>
+					<div className="bg-neutral-400 rounded-full w-10 h-10"></div>
+				</div>
+			</div>
+			<input
+				type="range"
+				defaultValue={speed}
+				min={0}
+				max={240}
+				onChange={(e) => setSpeed(parseInt(e.target.value))}
+			/>
+		</main>
+	);
 }
