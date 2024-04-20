324dad02b39665d1950ea003a0307459c97f4f14
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-20 10:54:12 +0530

    metervalue bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState, useRef } from "react";
 import gsap from "gsap";
 import { MeterSeven } from "@/components/MeterSeven";
 
-const green = ["#4ade80", "#22c55e", "#16a34a"];
-const defaults = ["#404040", "#525252", "#737373"];
-
 export default function Home() {
 	const [speed, setSpeed] = useState(0);
-
-	useEffect(() => {
-		const group = document.getElementById("six");
-
-		if (group) {
-			const paths = group.getElementsByTagName("path");
-
-			if (speed > 160) {
-				gsap.to(paths, {
-					fill: (i) => green[i],
-					stagger: {
-						each: 0.1,
-						from: "end",
-					},
-				});
-			} else {
-				gsap.to(paths, {
-					fill: (i) => defaults[i],
-					stagger: {
-						each: 0.1,
-						from: "start",
-					},
-				});
-			}
-		}
-	}, [speed]);
 	return (
 		<main className=" w-full h-screen flex flex-col gap-3 p-2 justify-center items-center bg-neutral-900">
 			<div className="h-[720px]  w-[1080px] bg-neutral-800 flex flex-col justify-center items-center rounded-3xl relative">
