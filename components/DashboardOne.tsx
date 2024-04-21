57016c22ae6d91accce3389eef7b98677f44ce9f
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-21 08:35:44 +0530

    dashboardone completed

--- components/DashboardOne.tsx
+++ components/DashboardOne.tsx
@@ -15,29 +15,59 @@ interface DashboardOneProps {
 }
 
 export const DashboardOne: React.FC<DashboardOneProps> = ({ speed }) => {
-  const textTwoRef = useRef<HTMLHeadingElement>(null);
-  const textRef = useRef<HTMLHeadingElement>(null);
   const hrRef = useRef<HTMLDivElement>(null);
   const [isLocation, setIsLocation] = useState(false);
 
+  const { animation: meterOneAnimation, elementRef: meterOneRef } = usePosition(
+    {
+      top: "50%",
+      left: "2%",
+      duration: 0.5,
+      scale: 0.5,
+    },
+  );
+  const { animation: meterTwoAnimation, elementRef: meterTwoRef } = usePosition(
+    {
+      top: "50%",
+      left: "20%",
+      duration: 0.5,
+      scale: 0.5,
+    },
+  );
+
+  const { animation: speedValueAnimation, elementRef: speedValueRef } =
+    usePosition({
+      top: "60%",
+      left: "25%",
+      duration: 0.5,
+      scale: 0.25,
+    });
+  const { animation: rpmValueAnimation, elementRef: rpmValueRef } = usePosition(
+    {
+      top: "60%",
+      left: "25%",
+      duration: 0.5,
+      scale: 0.25,
+    },
+  );
   useLayoutEffect(() => {
     function animate() {
       if (speed >= 0 && speed < 120) {
-        gsap.to([textRef.current, textTwoRef.current], {
+        gsap.to([speedValueRef.current, rpmValueRef.current], {
           color: "green",
         });
         gsap.to(hrRef.current, {
           backgroundColor: "green",
         });
       } else if (speed >= 120 && speed < 210) {
-        gsap.to([textRef.current, textTwoRef.current], {
+        gsap.to([speedValueRef.current, rpmValueRef.current], {
           color: "yellow",
         });
         gsap.to(hrRef.current, {
           backgroundColor: "yellow",
         });
       } else if (speed >= 210) {
-        gsap.to([textRef.current, textTwoRef.current], {
+        gsap.to([speedValueRef.current, rpmValueRef.current], {
           color: "red",
         });
         gsap.to(hrRef.current, {
@@ -51,39 +81,6 @@ export const DashboardOne: React.FC<DashboardOneProps> = ({ speed }) => {
 
     return () => ctx.kill();
   }, [speed]);
-
-  const { animation: meterOneAnimation, elementRef: meterOneRef } = usePosition(
-    {
-      y: 150,
-      x: -150,
-      duration: 0.5,
-      scale: 0.5,
-    },
-  );
-  const { animation: meterTwoAnimation, elementRef: meterTwoRef } = usePosition(
-    {
-      y: 150,
-      x: -550,
-      duration: 0.5,
-      scale: 0.5,
-    },
-  );
-
-  const { animation: speedValueAnimation, elementRef: speedValueRef } =
-    usePosition({
-      y: 500,
-      x: -432,
-      duration: 0.5,
-      scale: 0.25,
-    });
-  const { animation: rpmValueAnimation, elementRef: rpmValueRef } = usePosition(
-    {
-      y: 300,
-      x: -332,
-      duration: 0.5,
-      scale: 0.25,
-    },
-  );
 
   const { slideRef, slideAnimation } = useSlideInOut();
 
@@ -116,21 +113,24 @@ export const DashboardOne: React.FC<DashboardOneProps> = ({ speed }) => {
 
   return (
     <div className="h-[720px]  w-[1080px] overflow-hidden bg-neutral-800 flex flex-col justify-center items-center rounded-3xl relative">
-      <div className="absolute top-20 flex gap-5 flex-col w-[30%]">
-        <div className="flex flex-col  items-center z-20" ref={speedValueRef}>
-          <h1 ref={textRef} className="text-9xl text-green-500 ">
-            {speed}
-          </h1>
-          <h1 className="text-5xl text-white">Km/h</h1>
-        </div>
-        <div className="h-[1px] w-full bg-green-500" ref={hrRef}></div>
-        <div className="flex flex-col items-center z-20" ref={rpmValueRef}>
-          <h1 ref={textTwoRef} className="text-9xl z-20 text-green-500 ">
-            {speed}
-          </h1>
-          <h1 className="text-5xl text-white">RPM</h1>
-        </div>
-      </div>
+      <h1
+        ref={speedValueRef}
+        className="text-9xl z-20 absolute top-[40px] text-green-500 left-[50%] -translate-x-[50%]"
+      >
+        {speed}
+      </h1>
+      <h1 className="text-5xl text-white absolute top-[160px]">Km/h</h1>
+      <div
+        className="h-[1px] w-[30%] bg-green-500 absolute top-[215px]"
+        ref={hrRef}
+      ></div>
+      <h1
+        ref={rpmValueRef}
+        className="text-9xl z-20 text-green-500  absolute top-[220px] left-[50%] -translate-x-[50%] "
+      >
+        {speed}
+      </h1>
+      <h1 className="text-5xl text-white absolute top-[340px]">RPM</h1>
       <div
         className="w-[300px] absolute z-20 top-[40%] left-[10%]"
         ref={meterOneRef}
@@ -143,8 +143,15 @@ export const DashboardOne: React.FC<DashboardOneProps> = ({ speed }) => {
       >
         <MeterSeven speed={speed} />
       </div>
-      <div className="absolute w-20 -rotate-90 bottom-[20%]">
-        <Battery />
+      <div className="flex text-white  flex-col gap-1 items-center absolute bottom-[15%]">
+        <div className=" w-14  ">
+          <Battery />
+        </div>
+        <h1>Odometer - 11000 Miles</h1>
+        <div className="h-[1px] w-[150px] bg-green-500"></div>
+        <h1>Time - 3.00 pm</h1>
+        <div className="h-[1px] w-[150px] bg-green-500"></div>
+        <h1>Trip - 10000 Miles</h1>
       </div>
       <div className="flex gap-5 absolute bottom-5">
         <div className="bg-neutral-900 text-white rounded-full w-8 h-8 flex justify-center items-center">
--- hooks/usePosition.tsx
+++ hooks/usePosition.tsx
@@ -2,29 +2,29 @@ import gsap from "gsap";
 import { useLayoutEffect, useRef } from "react";
 
 interface PositionProps {
-  y: number;
-  x: number;
-  duration: number;
-  scale: number;
+	top: string;
+	left: string;
+	duration: number;
+	scale: number;
 }
 
-export const usePosition = ({ x, y, duration, scale }: PositionProps) => {
-  const elementRef = useRef<HTMLDivElement | HTMLHeadingElement>(null);
-  const animation = useRef<gsap.core.Timeline | undefined>();
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      animation.current = gsap.timeline({ paused: true });
-      animation.current.to(elementRef.current, {
-        y,
-        x,
-        duration,
-        scale,
-        transformOrigin: "center center",
-      });
-    });
+export const usePosition = ({ top, left, duration, scale }: PositionProps) => {
+	const elementRef = useRef<HTMLDivElement | HTMLHeadingElement>(null);
+	const animation = useRef<gsap.core.Timeline | undefined>();
+	useLayoutEffect(() => {
+		let ctx = gsap.context(() => {
+			animation.current = gsap.timeline({ paused: true });
+			animation.current.to(elementRef.current, {
+				top,
+				left,
+				duration,
+				scale,
+				transformOrigin: "center center",
+			});
+		});
 
-    return () => ctx.kill();
-  }, []);
+		return () => ctx.kill();
+	}, []);
 
-  return { animation, elementRef };
+	return { animation, elementRef };
 };
